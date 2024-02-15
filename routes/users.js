b0b3bbc23f54ffe244ca497270674c13b6305989
Alibaba <abdulmanafaliu414@gmail.com>
2024-02-15 11:24:48 +0000

    Minor changes

--- routes/users.js
+++ routes/users.js
@@ -10,9 +10,10 @@ router.get("/", function (req, res, next) {
 router.get("/cool", function (req, res, next) {
   res.send("You're so cool");
 });
-
+/*
 router.get("/bet", function (req, res, next) {
   res.send("I have stop betting");
 });
+*/
 
 module.exports = router;
