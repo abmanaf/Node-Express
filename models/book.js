1ce8c0cbd05e206ff3ccb1cffcd67d4dd90c10d8
Alibaba <abdulmanafaliu414@gmail.com>
2024-02-15 10:09:30 +0000

    created 4 js files

---
+++ models/book.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  title: { type: String, require: true },
+  author: { type: Schema.Types.ObjectId, ref: "Author", require: true },
+  summary: { type: String, require: true },
+  isbn: { type: String, require: true },
+  genre: [{ type: Schema.Types.ObjectId, ref: "Genre" }],
+});
+
+BookSchema.virtual("url").get(function () {
+  return `/models/book/${this_id}`;
+});
+
+module.exports = mongoose.model("Books", BookSchema);
---
+++ models/genre.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema({
+  name: { type: String, require: true, minLength: 3, maxLength: 100 },
+});
+
+GenreSchema.virtual("url").get(function () {
+  return `./models/genre/${this._id}`;
+});
+
+module.exports = mongoose.model("Genre", GenreSchema);
