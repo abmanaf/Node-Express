d35f96554b4a716f8521c263b3e944080b2bc565
Alibaba <abdulmanafaliu414@gmail.com>
2024-02-15 11:21:57 +0000

    created a new file

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const wiki = require("./routes/wiki.js");
 
 var app = express();
 const mongoose = require("mongoose");
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/wiki", wiki);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
