745ef4b9006195ab267f327c1cd3481326da3c9b
Alibaba <abdulmanafaliu414@gmail.com>
2024-02-17 09:32:53 +0000

    some small chnages

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -15,7 +15,6 @@ const BookInstanceSchema = new Schema({
 
 // Virtual for bookinstance's URL
 BookInstanceSchema.virtual("url").get(function () {
-  // We don't use an arrow function as we'll need the this object
   return `/catalog/bookinstance/${this._id}`;
 });
 
